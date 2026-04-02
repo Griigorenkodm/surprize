8efa2d660d920695c3ffebc0c97bce4e324453e9
Griigorenkodm <griigorenkodmr@gmail.com>
2026-04-02 21:38:54 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const btn1 = document.getElementById('first')
+const text1 =document.getElementById('tw')
+const btn2 = document.getElementById('second')
+const text2 =document.getElementById('thr')
+const btn3 = document.getElementById('three')
+const text3 =document.getElementById('fo')
+const btn4 = document.getElementById('four')
+const text4 =document.getElementById('fiv')
+const btn5 = document.getElementById('five')
+const text5 =document.getElementById('si')
+const btn6 = document.getElementById('six')
+const text6 =document.getElementById('sev')
+const btn7 = document.getElementById('seven')
+const text7 =document.getElementById('eig')
+const btn8 = document.getElementById('eight')
+const text8 =document.getElementById('nine')
+const btn9 = document.getElementById('long')
+const message = document.getElementById('span1')
+
+
+btn1.onclick = function(){
+text1.innerText = "ТЫ🧡"
+
+}
+
+
+btn2.onclick = function(){
+text2.innerText = "МОЯ🧡"
+
+}
+
+
+btn3.onclick = function(){
+text3.innerText = "САМАЯ💗💗💗"
+
+}
+
+
+
+btn4.onclick = function(){
+text4.innerText = "ЛЮБИМАЯ🧡🧡🧡🧡🧡"
+
+}
+
+
+btn5.onclick = function(){
+text5.innerText = "МАРМЕЛАДКА🧡🧡🧡"
+
+}
+
+
+btn6.onclick = function(){
+text6.innerText = "Я ТЕБЯ ЛЮБЛЮ🧡🧡🧡"
+
+}
+
+
+btn7.onclick = function(){
+text7.innerText = "БЕЗУМНО💋💋💋"
+
+}
+
+
+btn8.onclick = function(){
+text8.innerText = "ГО ЖЕНИТЬСЯ?)💗💗💗💗💗💗"
+
+}
+
+btn9.onclick = function() {
+
+    message.innerText = "Дашенька ты самая любимая моя мармеладка. Первый человек который меня любит и скучает по мне... Ты самая самая лучшая  Ты самая красивая и самая хорошая  В общем самая самая  Пиши мне о любой проблеме мой ангел хранитель Доверяй мне... Я не предам... Я люблю тебя безумно)"
+}
+
+
+
+
+
+
